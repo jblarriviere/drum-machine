92cb3a1b564f0dc4d150e3cc1df45b4e68ce197b
Jean-Baptiste LARRIVIERE <jb.larriviere@laposte.net>
2021-03-11 22:34:38 +0100

    Fulfilling user story 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import './App.css';
+import React from 'react';
+
+const audios = {
+  drums: {
+    Q: "Heater 1",
+    W: "Heater 2",
+    E: "Heater 3",
+    A: "Heater 4",
+    S: "Clap",
+    D: "Open HH",
+    Z: "Kick n' Hat",
+    X: "Kick",
+    C: "Closed HH"
+  },
+  piano: {
+    Q: "Chord 1",
+    W: "Chord 2",
+    E: "Chord 3",
+    A: "Shaker",
+    S: "Open HH",
+    D: "Closed HH",
+    Z: "Punchy Kick",
+    X: "Side Stick",
+    C: "Snare"
+  }
+};
 
 class App extends React.Component {
   
   constructor(props) {
     super(props);
-    this.state= {
-
+    this.state = {
+      mode: 'drums'
     };
   }
   
   render() {
+
+    const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "D"];
+    const pads = keys.map(key => 
+      <Pad className="drum-pad" id={audios[this.state.mode][key]} keyName={key} />
+    );
+
     return (
       <div id="drum-machine">
-        
+        {pads}
         <div id="display"></div>
       </div>
     );
   }
 }
 
+function Pad(props) {
+  return(
+    <button>{props.keyName}</button>
+  );
+}
+ 
 export default App;
