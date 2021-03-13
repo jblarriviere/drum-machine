f5b5f4315ad435f57f429479031ddcd3fde4623f
Jean-Baptiste LARRIVIERE <jb.larriviere@laposte.net>
2021-03-13 19:05:51 +0100

    Creating the static app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-constructor */
 import './App.css';
 import React from 'react';
 
@@ -12,17 +13,6 @@ const audios = {
     Z: "Clave",
     X: "Kick",
     C: "Closed_HH"
-  },
-  piano: {
-    Q: "Chord 1",
-    W: "Chord 2",
-    E: "Chord 3",
-    A: "Shaker",
-    S: "Open HH",
-    D: "Closed HH",
-    Z: "Punchy Kick",
-    X: "Side Stick",
-    C: "Snare"
   }
 };
 
@@ -31,34 +21,98 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'drums',
-      display: 'Lets start'
+      display: "Affichage"
     };
   }
-
-  
   render() {
-
-    const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "D"];
-    const pads = keys.map(key => 
-      <Pad className="drum-pad" id={audios[this.state.mode][key]} keyName={key} />
-    );
-
     return (
       <div id="drum-machine">
-        {pads}
-        <div id="display">{this.state.display}</div>
+        <PadControls />
+        <SettingControls display={this.state.display}/>
       </div>
     );
   }
 }
 
-function Pad(props) {
-  return(
-    <button>{props.keyName}
-      <audio src={"../audio/"+props.id+".wav"} className="clip" id={props.keyName} />
-    </button>
-  );
+class PadControls extends React.Component {
+
+  render() {
+    const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "D"];
+    const pads = keys.map(key => 
+      <Pad className="drum-pad" 
+          id={audios.drums[key]}
+          keyName={key} 
+      />
+    );
+    return(
+      <div id="padControls">
+        {pads}
+      </div>
+    );
+  }
 }
+
+class Pad extends React.Component {
+  render() {
+    return(
+      <button>{this.props.keyName}
+        <audio src={"../audio/"+this.props.id+".wav"}
+              className="clip"
+              id={this.props.keyName} 
+        />
+      </button>
+    );
+  }
+}
+
+class SettingControls extends React.Component {
+  render() {
+    return(
+      <div id="settingControls">
+        <Switch name='Power' />
+        <Display text={this.props.display}/>
+        <VolumeSlide />
+        <Switch name='Bank' />
+      </div>
+    );
+  }
+}
+
+class Switch extends React.Component {
+  render() {
+    return(
+      <div className="switchContainer">
+        <p>{this.props.name}</p>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </div>
+    );
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    return(
+      <p>{this.props.text}</p>
+    );
+  }
+}
+
+class VolumeSlide extends React.Component {
+  render() {
+    return(
+      <div id="volumeSlide">
+        <input type="range" 
+              id="volume" 
+              name="volume"
+        />
+        <label for="volume">Volume</label>
+      </div>
+    );
+  }
+}
+
  
 export default App;
